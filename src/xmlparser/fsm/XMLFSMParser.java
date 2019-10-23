15ea30fd68d6202dfc4299a650fc2d4f0df8768d
bd062554 <bd062554@iq-147-13.iut21.u-bourgogne.fr>
2019-10-23 15:13:43 +0200

    FSM start point

---
+++ src/xmlparser/fsm/XMLFSMParser.java
@@ -0,0 +1,11 @@
+package xmlparser.fsm;
+
+/**
+ * XML file parsing class using a FSM
+ * @author bd062554
+ */
+public class XMLFSMParser {
+    public static void startXMLParser() {
+        
+    }
+}
