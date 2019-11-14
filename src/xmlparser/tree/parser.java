9bc9069ec42ee1f8e3b137f9ce3f430192631226
tj868158 <tj868158@iq-r24-09.iut21.u-bourgogne.fr>
2019-11-14 15:27:35 +0100

    Automate

--- src/xmlparser/tree/parser.java
+++ src/xmlparser/tree/parser.java
@@ -10,5 +10,50 @@ package xmlparser.tree;
  * @author rm792745
  */
 public class parser {
-    private node baliseCourante;
+    private Node baliseCourante = null;
+    private Tree arbre = null;
+    
+    public void createXMLtree(String fileContent)
+    {
+        arbre = new Tree();
+        String tmp = "";
+        
+        for(int i = 0; i< fileContent.length();i++){
+            switch(fileContent.charAt(i)){
+                case '<' : 
+                    if(fileContent.charAt(i+1) == '/'){
+                        baliseCourante = baliseCourante.getParent();
+                    }
+                    else if(arbre.getRacine() == null){
+                        baliseCourante = new Node(null);
+                        arbre.setRacine(baliseCourante);
+                    }
+                    else
+                    {
+                        Node newBalise = new Node(baliseCourante);
+                        baliseCourante.addFils(newBalise);
+                        Node oldBalise = baliseCourante;
+                        baliseCourante = newBalise;
+                        baliseCourante.setParent(oldBalise);
+                    }
+                break;
+                case '>':
+                    if (baliseCourante.getNom().equals("")) {
+                        baliseCourante.setNom(tmp);
+                        tmp = "";
+                    } else if (baliseCourante.getContenu().equals("")) {
+                            baliseCourante.setContenu(tmp);
+                            tmp = "";
+                    } else {
+                        baliseCourante = baliseCourante.getParent();
+                    }
+                break;
+                default :
+                    tmp += (fileContent.charAt(i-1) != '<' && fileContent.charAt(i) == '/') ? "" : fileContent.charAt(i);
+                break;
+            }
+        }
+        System.out.println("finished");
+        tmp = "";
+    }
 }
